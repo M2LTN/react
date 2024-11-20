b01e8da1868fba8557890d3fab6a662f5127028c
aaron <aaron.malton@voco.ee>
2024-11-20 11:09:38 +0200

    Komponendi sisustamine andmetega

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
 import ExpenseItem from './components/ExpenseItem';
-const App = () => {
+
+function App() {
+  const expenses = [
+    { date: "2024-01-01", title: "Groceries", price: "$94.12" },
+    { date: "2024-01-02", title: "Rent", price: "$450.00" },
+    { date: "2024-01-03", title: "Electricity Bill", price: "$30.00" },
+  ];
+
   return (
-    <div
-      className="App">
-      <ExpenseItem />
+    <div>
+      {expenses.map((expense, index) => (
+        <ExpenseItem 
+          key={index}
+          date={expense.date} 
+          title={expense.title} 
+          price={expense.price} 
+        />
+      ))}
     </div>
   );
-};
-
+}
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,18 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-    return (
-            <div className='expensive-item'>
-            <div>Date</div>
-            <div className='expense-item_description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item_price'> Price</div>
-            </div>
-        </div>
-
-    )
+const ExpenseItem = (props) => {
+    const date = new Date(2024, 11, 20);
+    const title = 'Car Insurance';
+    const price = 294.67;
+  return (
+    <div className="expense-item">
+      <div>{props.date}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.price}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
