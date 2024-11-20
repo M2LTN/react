4fd5c3d84dfbdac208c6b23ebf0855f516919d4e
aaron <aaron.malton@voco.ee>
2024-11-20 11:36:51 +0200

    Komponendi jaotamine mitmeks komponendiks

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    const date = new Date(2024, 11, 20);
-    const title = 'Car Insurance';
-    const price = 294.67;
   return (
     <div className="expense-item">
-      <div>{props.date}</div>
+        <ExpenseDate date={props.data.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.price}</div>
